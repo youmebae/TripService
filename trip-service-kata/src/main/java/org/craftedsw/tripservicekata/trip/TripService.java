145a97e295fc3862caafd37c3edf96a12587b72c
배유미 <youme.bae@samsung.com>
2017-07-21 19:17:30 +0900

    LoggedInUser를 파라미터로 전달

--- trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -5,16 +5,15 @@ import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		if(getLoggedInUser() == null)	{
+	public List<Trip> getTripsByUser(User user, User loggedInUser) throws UserNotLoggedInException {
+		if(loggedInUser == null)	{
 			throw new UserNotLoggedInException();
 		}
 		
- 		return user.isFriendsWith(getLoggedInUser())
+ 		return user.isFriendsWith(loggedInUser)
  				? tripsBy(user)
  				: noTrips();
 	}
@@ -25,10 +24,6 @@ public class TripService {
 
 	protected List<Trip> tripsBy(User user) {
 		return TripDAO.findTripsByUser(user);
-	}
-
-	protected User getLoggedInUser() {
-		return UserSession.getInstance().getLoggedUser();
 	}
 	
 }

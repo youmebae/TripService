fa5671c8cf824839d0558fec937ed85eef9dde8d
배유미 <youme.bae@samsung.com>
2017-07-21 18:13:39 +0900

    User class에 isFriendsWith 메소드 추가

--- trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,35 @@
 package org.craftedsw.tripservicekata.user;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.craftedsw.tripservicekata.trip.UserBuilder;
+import org.junit.Test;
+
 public class UserTest {
+	
+	private static final User BOB = new User();
+	private static final User PAUL = new User();
+
+	@Test
+	public void should_inform_when_users_are_not_friends() 
+			throws Exception {
+		User user = UserBuilder.aUser()
+				.friendsWith(BOB)
+				.build();
+		
+		assertThat(user.isFriendsWith(PAUL), is(false));
+	}
+	
+	@Test
+	public void should_inform_when_users_are_friends() 
+			throws Exception {
+		User user = UserBuilder.aUser()
+				.friendsWith(BOB, PAUL)
+				.build();
+		
+		assertThat(user.isFriendsWith(PAUL), is(true));
+	}
 
 }
